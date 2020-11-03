af151c8d8af3619b08f855bc3a77756aacdfd799
Jiang Hao <giovannilance94@gmail.com>
2020-11-03 20:27:35 +0800

    修改其他部分 twitter 描述

--- site/src/routes/docs/index.svelte
+++ site/src/routes/docs/index.svelte
@@ -1,22 +1,22 @@
 <script context="module">
-	export async function preload() {
-		const sections = await this.fetch(`docs.json`).then((r) => r.json());
-		return { sections };
-	}
+  export async function preload() {
+    const sections = await this.fetch(`docs.json`).then((r) => r.json())
+    return { sections }
+  }
 </script>
 
 <script>
-	import { Docs } from "@sveltejs/site-kit";
+  import { Docs } from '@sveltejs/site-kit'
 
-	export let sections;
+  export let sections
 </script>
 
 <svelte:head>
-	<title>API 文档 • Svelte</title>
+  <title>API 文档 • Svelte</title>
 
-	<meta name="twitter:title" content="Svelte API docs" />
-	<meta name="twitter:description" content="Cybernetically enhanced web apps" />
-	<meta name="Description" content="Cybernetically enhanced web apps" />
+  <meta name="twitter:title" content="Svelte API docs" />
+  <meta name="twitter:description" content="赛博强化型 web 应用程序" />
+  <meta name="Description" content="赛博强化型 web 应用程序" />
 </svelte:head>
 
 <Docs {sections} />
